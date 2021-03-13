d4321b95b602051a634b3058424777a806cff2d1
Roni Kurniawan <rokurnia@microsoft.com>
2021-03-13 10:42:10 +0700

    Add project files.

---
+++ WebAppKeyVault/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Security;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebAppKeyVault.Helpers;
+
+namespace WebAppKeyVault.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            var dbUserName = "DbUserName";
+            var dbPassword = "DbPassword";
+
+            var secrets = await KeyVaultReader.ReadSecrets(new string[] { dbUserName, dbPassword });
+            var dbUserNameValue = secrets[dbUserName];
+            var dbPasswordValue = secrets[dbPassword];
+
+            ViewBag.Message = $"Value from Azure Key Vault";
+            ViewBag.MessageLine1 = $"{dbUserName}: {dbUserNameValue}";
+            ViewBag.MessageLine2 = $"{dbPassword}: {dbPasswordValue}";
+
+
+
+            // Modify credential of ConnectionString
+            var connString = ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString;
+            var conn = new SqlConnection(connString);
+
+            var securePassword = new SecureString();
+            foreach (char character in dbPasswordValue)
+            {
+                securePassword.AppendChar(character);
+            }
+            securePassword.MakeReadOnly();
+            conn.Credential = new SqlCredential(dbUserNameValue, securePassword);
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+    }
+}
---
+++ WebAppKeyVault/Helpers/KeyVaultReader.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAppKeyVault.Helpers
+{
+    public class KeyVaultReader
+    {
+        public static async Task<string> ReadSecret(string keyName)
+        {
+            var keyVaultEndpoint = ConfigurationManager.AppSettings["KeyValutEndpoint"];
+            var tenantId = ConfigurationManager.AppSettings["TenantId"];
+            var appId = ConfigurationManager.AppSettings["AppId"];
+            var appSecret = ConfigurationManager.AppSettings["AppSecret"];
+
+            var tokenConnectionString = $"RunAs=App;AppId={appId};TenantId={tenantId};AppKey={appSecret}";
+            var azureServiceTokenProvider = new AzureServiceTokenProvider(tokenConnectionString);
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            var secret = await keyVaultClient.GetSecretAsync(keyVaultEndpoint, keyName, CancellationToken.None);
+            return secret.Value;
+        }
+
+
+        public static async Task<IDictionary<string, string>> ReadSecrets(IEnumerable<string> keyNames)
+        {
+            var secrets = new Dictionary<string, string>();
+
+            var keyVaultEndpoint = ConfigurationManager.AppSettings["KeyValutEndpoint"];
+            var tenantId = ConfigurationManager.AppSettings["TenantId"];
+            var appId = ConfigurationManager.AppSettings["AppId"];
+            var appSecret = ConfigurationManager.AppSettings["AppSecret"];
+
+            var tokenConnectionString = $"RunAs=App;AppId={appId};TenantId={tenantId};AppKey={appSecret}";
+            var azureServiceTokenProvider = new AzureServiceTokenProvider(tokenConnectionString);
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            foreach(var keyName in keyNames)
+            {
+                var secret = await keyVaultClient.GetSecretAsync(keyVaultEndpoint, keyName, CancellationToken.None);
+                secrets.Add(keyName, secret.Value);
+            }
+
+            return secrets;
+        }
+
+
+    }
+}
